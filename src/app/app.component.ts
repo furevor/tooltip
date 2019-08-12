02ddaf222c2b6c262a77d0cb5c7dca62e575b0a1
Vladimir Erygin <furevor@gmail.com>
2019-08-12 22:58:20 +0300

    bext timeouts solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,28 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {fromEvent, Observable, of, Subject, timer} from 'rxjs';
-import {debounceTime, delay, exhaustMap, filter, switchMap, takeUntil, tap, throttleTime} from 'rxjs/operators';
+import { Component, ViewChild } from '@angular/core';
+import { TooltipDirective } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
-  title = 'tooltip';
-  content = 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus.';
-  tooltipShowEvents$: Subject<string> = new Subject();
-  hintMouseEvents$: Subject<string> = new Subject();
-  @ViewChild('pop', { static: false }) tooltip;
+export class AppComponent {
 
-  constructor() {}
+  tooltipMessage: string;
+  @ViewChild('pop', { static: false }) tooltip: TooltipDirective;
 
-  ngAfterViewInit(): void {}
+  timer;
 
-  tooltipMouseLeave() {
-    const delayHintClose = this.hintMouseEvents$.pipe(
-      filter(event => event === 'enter'),
-      tap(console.log),
-    );
-    timer(800).pipe(takeUntil(delayHintClose)).subscribe(() => this.tooltip.hide());
+  hide() {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.tooltip.hide(), 800);
   }
 
-  tooltipMouseEnter() {
-    this.tooltipShowEvents$.next('show');
-    const delayHintClose = this.hintMouseEvents$.pipe(
-      filter(event => event === 'enter'),
-      tap(console.log),
-    );
-  }
-
-  tooltipOnShow() {
-    this.tooltipShowEvents$.next('show');
-  }
-
-  tooltipOnHide() {
-    this.tooltipShowEvents$.next('hide');
-  }
-
-  hintEnter($event) {
-    this.hintMouseEvents$.next('enter');
-  }
-
-  hintLeave($event) {
-    timer(800).subscribe(() => this.tooltip.hide());
-    this.hintMouseEvents$.next('leave');
+  show() {
+    clearTimeout(this.timer);
+    if (this.tooltip.isOpen) {
+      return;
+    }
+    this.tooltip.show();
   }
 }
